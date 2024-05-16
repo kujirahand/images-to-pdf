d2470c9b350ff58c324e0cb331871f6ae1bce673
kujirahand <web@kujirahand.com>
2024-05-16 19:22:34 +0900

    modified Bat file

--- conv-to-exe.bat
+++ conv-to-exe.bat
@@ -1,4 +1,4 @@
-pyinstaller images-to-pdf.py --onefile
+pyinstaller images-to-pdf.py --onefile --noconsole
 copy README.md dist\README.txt
 mkdir dist\pdf-in
 copy pdf-in\test.heic dist\pdf-in\test.heic
